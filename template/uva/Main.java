837f539bbf717298a245055bd1d11c802bff5e91
Gobinda Paul <gobinda.paul.1547@gmail.com>
2022-12-18 20:35:04 +0600

    adding uva template

---
+++ template/uva/Main.java
@@ -0,0 +1,83 @@
+package uva;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class TaskSolver {
+
+		public static void solve(MyScanner in, MyWriter out) throws IOException {
+
+			int x = in.nextInt();
+			System.out.println(x);
+
+		}
+
+	}
+
+	public static void main(String[] arg) {
+		try {
+			MyScanner in = new MyScanner();
+			MyWriter out = new MyWriter();
+			TaskSolver.solve(in, out);
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	static class MyWriter extends PrintWriter {
+		MyWriter() throws FileNotFoundException {
+			super(System.out);
+		}
+	}
+
+	static class MyScanner {
+
+		private BufferedReader br;
+		private StringTokenizer st;
+
+		public MyScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public String next() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (Exception e) {
+					return null;
+				}
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		public String nextLine() {
+			try {
+				return br.readLine();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+	}
+
+}
